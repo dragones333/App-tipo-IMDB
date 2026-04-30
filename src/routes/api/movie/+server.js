45b1ca2e4535ff36bbd1df13a3404f26bc553e7d
Saristeidyn <129422890+Saristeidyn@users.noreply.github.com>
2026-04-30 14:06:10 -0600

    made the filters for movie

--- src/routes/api/movie/+server.js
+++ src/routes/api/movie/+server.js
@@ -1,14 +1,13 @@
-import { 
-    createMovie, 
-    getMovies 
-} from "$lib/models/movie.js";
 
 import { json } from "@sveltejs/kit";
+import { getFilteredMovies, createMovie } from "$lib/models/movie.js";
 
+export async function GET({ url }) {
+    const rating = url.searchParams.get("rating") || null;
+const category = url.searchParams.get("category") || null;
+const actor = url.searchParams.get("actor") || null;
 
-// GET ALL MOVIES
-export async function GET(){
-    const res = await getMovies();
+    const res = await getFilteredMovies({ rating, category, actor });
     return json(res);
 }
 
@@ -29,3 +28,33 @@ export async function POST({ request }){
 
     return json(res);
 }
+
+
+
+
+// # BASE (no filters)
+// http://localhost:5173/api/movie
+
+// # RATING
+// http://localhost:5173/api/movie?rating=8
+
+// # CATEGORY
+// http://localhost:5173/api/movie?category=1
+
+// # ACTOR
+// http://localhost:5173/api/movie?actor=5
+
+
+// # COMBINATIONS
+
+// # rating + category
+// http://localhost:5173/api/movie?rating=8&category=1
+
+// # rating + actor
+// http://localhost:5173/api/movie?rating=8&actor=5
+
+// # category + actor
+// http://localhost:5173/api/movie?category=1&actor=5
+
+// # ALL FILTERS
+// http://localhost:5173/api/movie?rating=8&category=1&actor=5
