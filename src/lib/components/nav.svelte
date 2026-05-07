0d2fc42f9639991109a8d385eb3324615dd822e1
Denisse Chagoya <denissechagoyaveloz@gmail.com>
2026-05-07 16:34:37 -0600

    fix: remove remaining search bar code

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -32,16 +32,7 @@
 
         <!-- ACTIONS -->
         <div class="actions">
-            <div class="search">
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                    viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                    stroke-width="2">
-                    <circle cx="11" cy="11" r="8"></circle>
-                    <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-                </svg>
-
-                
-            </div>
+            
 
             
 
